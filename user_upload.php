43645a0187c908f011e3a00b4c0c1b24067cc1a8
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-27 21:31:22 +1000

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -1,32 +1,17 @@
-
-
 <?php 
-require "database.php";
 
-$lines = file('users.csv', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-$csv = array_map('str_getcsv', $lines);
-$col_names = array_shift($csv);
+$hostname = 'localhost';
+$username = 'root';
+$password = 'yourpasswordhere';
 
-$users = [];
 try {
-foreach($csv as $row) {
-    $users[] = [
-        $col_names[0] => ucfirst(strtolower($row[0])),
-        $col_names[1] => ucfirst(strtolower($row[1])),
-        $col_names[2] => strtolower($row[2]),
-    ];
-  
-    $stmt = $dbh->prepare("INSERT INTO users(name, surname, email) 
-            VALUES(:name, :surname, :email)");
-            $stmt->bindParam(':name', $col_names[0]);
-            $stmt->bindParam(':surname',$col_names[1]);
-            $stmt->bindParam(':email', $col_names[2]);
-            $stmt->execute();
-    }
+    $dbh = new PDO("mysql:host=$hostname;dbname=userInformation", $username, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Connected to Database<br/>';
+
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }
-catch(PDOException $e) {
-    echo "There is an duplicate email in the file. \r\n Error: " . $e->getMessage();
-}
-$dbh = null;
+
 
 ?>
