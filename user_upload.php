fb4efe776eade6fcb80e6c95d0e997c48120c251
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-28 08:29:25 +1000

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -1,6 +1,7 @@
-<?php 
-require 'database.php';
 
+
+<?php 
+require "database.php";
 
 
 $options = ['file:'];
@@ -8,25 +9,28 @@ $options = ['file:'];
 $values = getopt(null, $options);
 
 $lines = file($values['file'], FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-$csv = array_map('str_getcsv', $lines);
 
+
+$csv = array_map('str_getcsv', $lines);
+$col_names = array_shift($csv);
 
 $users = [];
-$stmt = $dbh->prepare('INSERT INTO users (name, surname, email) VALUES (?,?,?)'); 
 foreach($csv as $row) {
     $users[] = [
         $col_names[0] => ucfirst(strtolower($row[0])),
         $col_names[1] => ucfirst(strtolower($row[1])),
-        $col_names[2] => strtolower(strtolower($row[2])),
+        $col_names[2] => filter_var(strtolower($row[2]), FILTER_VALIDATE_EMAIL),
     ];
+   
+    $stmt = $dbh->prepare("INSERT INTO users(name, surname, email) 
+            VALUES(:name, :surname, :email)");
+            $stmt->bindParam(':name', $col_names[0]);
+            $stmt->bindParam(':surname', $col_names[1]);
+            $stmt->bindParam(':email', $col_names[2]);
+            $stmt->execute();
+      
 }
-
-foreach($users as $user) {
-    $stmt->execute($user);
-}
+$dbh = null;
 
 ?>
-
-
-
 
