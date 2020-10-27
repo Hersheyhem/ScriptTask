b8511a4a4d418c239cb756841a3e6f586d8a90a9
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-28 09:00:03 +1000

    Update database.php
    Indent the code

--- database.php
+++ database.php
@@ -1,17 +1,11 @@
 <?php 
-
 $hostname = 'localhost';
 $username = 'root';
-$password = 'yourpasswordhere';
-
+$password = 'hemlata96';
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=userDetails", $username, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connected to Database';
-
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
-
-
 ?>
