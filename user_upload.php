ef99251d331b582fc5273a4594768b7ea3e7c2bb
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-28 08:11:26 +1000

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -1,17 +1,32 @@
 <?php 
+require 'database.php';
 
-$hostname = 'localhost';
-$username = 'root';
-$password = 'yourpasswordhere';
 
-try {
-    $dbh = new PDO("mysql:host=$hostname;dbname=userInformation", $username, $password);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Connected to Database<br/>';
 
-} catch (PDOException $e) {
-    echo $e->getMessage();
+$options = ['file:'];
+
+$values = getopt(null, $options);
+
+$lines = file($values['file'], FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+$csv = array_map('str_getcsv', $lines);
+
+
+$users = [];
+$stmt = $dbh->prepare('INSERT INTO users (name, surname, email) VALUES (?,?,?)'); 
+foreach($csv as $row) {
+    $users[] = [
+        $col_names[0] => ucfirst(strtolower($row[0])),
+        $col_names[1] => ucfirst(strtolower($row[1])),
+        $col_names[2] => strtolower(strtolower($row[2])),
+    ];
 }
 
+foreach($users as $user) {
+    $stmt->execute($user);
+}
 
 ?>
+
+
+
+
