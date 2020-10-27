4f4a5b9a2ef8206a718cd0249a7a85c6893f8d98
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-27 20:12:07 +1000

    Create user_upload.php

---
+++ user_upload.php
@@ -0,0 +1,28 @@
+<?php 
+require "database.php";
+
+$lines = file('users.csv', FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
+$csv = array_map('str_getcsv', $lines);
+$col_names = array_shift($csv);
+
+$users = [];
+foreach($csv as $row) {
+    $users[] = [
+        $col_names[0] => ucfirst(strtolower($row[0])),
+        $col_names[1] => ucfirst(strtolower($row[1])),
+        $col_names[2] => strtolower(strtolower($row[2])),
+    ];
+    try {
+    $stmt = $dbh->prepare("INSERT INTO users(name, surname, email) 
+            VALUES(:name, :surname, :email)");
+            $stmt->bindParam(':name', $col_names[0]);
+            $stmt->bindParam(':surname',$col_names[1]);
+            $stmt->bindParam(':email', $col_names[2]);
+            $stmt->execute();
+    }catch(PDOException $e) {
+        echo "There is an duplicate email in the file. \r\n Error: " . $e->getMessage();
+    }
+}
+$dbh = null;
+
+?>
