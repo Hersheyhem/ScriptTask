cef0b37f7556b95436dc7a1c4d0c0314d4b4d87b
Hemlata Mahaur <hemlatamahaur96@gmail.com>
2020-10-28 08:30:41 +1000

    Update database.php

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 
 $hostname = 'localhost';
 $username = 'root';
-$password = 'hemlata96';
+$password = 'yourpasswordhere';
 
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=userDetails", $username, $password);
